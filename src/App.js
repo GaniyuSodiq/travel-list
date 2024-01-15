0ef9c0db170fa8dfb4c2a1449603a3c971e5ce47
Sodiq Ganiyu <gsolaniyi@gmail.com>
2024-01-15 20:26:23 +0100

    building a form and handling submissions

--- src/App.js
+++ src/App.js
@@ -19,10 +19,23 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="text..." />
+      <button>ADD</button>
+    </form>
   );
 }
 function ParkingList() {
@@ -30,7 +43,7 @@ function ParkingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
