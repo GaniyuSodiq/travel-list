3cb6f250f2dfb5f8a2fbd496e8959d8bd42a6165
Sodiq Ganiyu <gsolaniyi@gmail.com>
2024-01-15 18:20:24 +0100

    complete the static part

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-export default function App() {}
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <ParkingList />
+      <Stats />
+    </div>
+  );
+}
 
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
-function Form() {}
-function ParkingList() {}
-function Stats() {}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+function ParkingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items in your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
